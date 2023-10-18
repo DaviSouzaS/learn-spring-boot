4fc76f5f5dbbabae4acf455e5a4a8f492cfee6b9
davi <daavisouza.santana@gmail.com>
2023-10-18 17:42:01 -0300

    create spring boot web project

---
+++ src/main/java/dss/learnjavaweb/LearnJavaWebApplication.java
@@ -0,0 +1,13 @@
+package dss.learnjavaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dss/learnjavaweb/LearnJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dss.learnjavaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
