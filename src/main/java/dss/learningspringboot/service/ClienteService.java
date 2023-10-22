7140b17ee6b86e5882ab4b40e4ebb3d0e849b6f6
davi <daavisouza.santana@gmail.com>
2023-10-21 22:37:21 -0300

    create basic spring boot API

---
+++ src/main/java/dss/learningspringboot/service/ClienteService.java
@@ -0,0 +1,17 @@
+package dss.learningspringboot.service;
+
+import dss.learningspringboot.model.Cliente;
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
--- src/test/java/dss/learningspringboot/LearningSpringBootApplicationTests.java
+++ src/test/java/dss/learningspringboot/LearningSpringBootApplicationTests.java
@@ -1,10 +1,10 @@
-package dss.learnjavaweb;
+package dss.learningspringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LearnJavaWebApplicationTests {
+class LearningSpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
